a63e3aafa10451cb68f81a900a726db4c132009a
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-20 13:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function screensize(){
+
+    const head = document.getElementById("sizeInfo");
+    const w = window.innerWidth;
+    const h =window.innerHeight;
+    head.innerHTML = `<h1>Width: ${w} and Height: ${h} </h1>`;
+    
+    
+
+}
+
+window.addEventListener("resize",screensize);
+
+screensize();
